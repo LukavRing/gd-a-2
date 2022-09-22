5280e85906a34f46b74418ca278d59bc5fe3c2cd
LukavRing <112349619+LukavRing@users.noreply.github.com>
2022-09-22 10:56:11 +0200

    EnemyScripts

--- Workshop3/Assets/Scripts/EnemyBehaviour.cs
+++ Workshop3/Assets/Scripts/EnemyBehaviour.cs
@@ -1,33 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     private NavMeshAgent myAgent;
-    private GameObject[] wanderPoints;
-
+    public GameObject[] wanderPoints;
     private Transform headingPoint;
+
+    public float counterTime = 5f;
+    float counter = 0f;
     // Start is called before the first frame update
     void Start()
     {
         myAgent = GetComponent<NavMeshAgent>();
         wanderPoints = GameObject.FindGameObjectsWithTag("WanderPoint");
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-    void startWander(){
+    public void StartWander()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        setHeading();
+    }
+    public void FixedUpdateWaner()
+    {
         
     }
-    void fixedUpdateWaner(){
-   
+    public void UpdateWander(){
+        counter += 1f * Time.deltaTime;
+        if (counter >= counterTime)
+        {
+            setHeading();
+            counter = 0;
+        }
     }
-    void UpdateWander(){
+
+    public void StartGoToPlayer()
+    {
         
+    }
+
+    public void UpdateGotoPlayer()
+    {
+        
+    }
+
+    private void setHeading()
+    {
+        int choice = Random.Range(0, wanderPoints.Length -1);
+        headingPoint = wanderPoints[choice].transform;
+        myAgent.SetDestination(headingPoint.position);
     }
 }
